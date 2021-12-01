18810ab4064c0b7cc2399604a0be6c5eec2a7057
Adriano2397 <adriano.cappelli@hs-furtwangen.de>
2021-12-01 15:11:30 +0100

    Delelte Button

--- Aufgabe4/script.ts
+++ Aufgabe4/script.ts
@@ -1,39 +1,71 @@
 namespace Eventtabelle {
 
-  const interpretInput: HTMLInputElement = <HTMLInputElement>document.getElementById("interpretInput"); 
-  const priceInput: HTMLInputElement = <HTMLInputElement>document.getElementById("priceInput"); 
-  const display: HTMLElement = <HTMLElement>document.getElementById("display"); 
-  const myButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("enterButton"); 
-  
-  myButton.addEventListener("click", mybuttonHandler); 
-      
-  class Event {
-    private interpret: string;
-    private price: number;
-    constructor(interpret: string, price: number){
-      this.interpret = interpret;
-      this.price = price;
+  const interpretInput: HTMLInputElement = <HTMLInputElement>document.getElementById("interpretInput");
+  const priceInput: HTMLInputElement = <HTMLInputElement>document.getElementById("priceInput");
+  const display: HTMLElement = <HTMLElement>document.getElementById("display");
+  const myButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("enterButton");
+  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById("inputElement");
+  const savebutton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("savebutton");
+  const loadbutton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("loadbutton");
+
+
+  savebutton.addEventListener("click", saveButtonHandler);
+  loadbutton.addEventListener("click", loadButtonHandler);
+
+  function saveButtonHandler(): void {
+    let speicher: number[] = [];
+    let arrayLocalStorage: number[];
+    let arrayString: string = JSON.stringify(speicher)
+
+
+    function loadButtonHandler(): void {
+      let valueFromLocalStorage: string = localStorage.getItem("gis_praktikum_input");
+      console.log("aktuelle Wert im Local Storage: " + valueFromLocalStorage);
+      display.textContent = valueFromLocalStorage;
+      localStorage.setItem("myArray", arrayString);
+      let stringLocalStorage: string = localStorage.getItem("myArray")
+      arrayLocalStorage = JSON.parse(stringLocalStorage);
+
+
     }
 
-    tabelleEintragen(): void{
-      
 
-    }
-  }
-  
-  
 
-  function mybuttonHandler(){
-    let interpretValue: string = interpretInput.value;
-    let priceValue: number = Number(priceInput.value);
-    const newZeile: HTMLTableRowElement = document.createElement("tr")
+    myButton.addEventListener("click", mybuttonHandler);
+
+
+    function mybuttonHandler() {
+      let interpretValue: string = interpretInput.value;
+      let priceValue: number = Number(priceInput.value);
+      let deleteButton: HTMLButtonElement = document.createElement("button");
+      deleteButton.textContent = "Delete";
+
+
+
+      const newZeile: HTMLTableRowElement = document.createElement("tr")
       const newInterpret: HTMLTableCellElement = document.createElement("td");
       newInterpret.textContent = interpretValue;
       const newPrice: HTMLTableCellElement = document.createElement("td");
       newPrice.textContent = String(priceValue);
 
+      deleteButton.addEventListener("click", deleteEvent);
+
+
       display.appendChild(newZeile);
       newZeile.appendChild(newInterpret);
       newZeile.appendChild(newPrice);
+      newZeile.appendChild(deleteButton);
+
+
+      function deleteEvent(): void {
+        newZeile.removeChild(newInterpret);
+        newZeile.removeChild(newPrice);
+        newZeile.removeChild(deleteButton);
+
+
+      }
+
+    }
   }
+
 }
