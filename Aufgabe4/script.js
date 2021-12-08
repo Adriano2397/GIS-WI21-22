e6688a9e79c3dc88fcf46c704fc39a49edefb1c0
Adriano2397 <adriano.cappelli@hs-furtwangen.de>
2021-12-08 09:16:42 +0100

    LocaLStorgarage

--- Aufgabe4/script.js
+++ Aufgabe4/script.js
@@ -5,44 +5,60 @@ var Eventtabelle;
     const priceInput = document.getElementById("priceInput");
     const display = document.getElementById("display");
     const myButton = document.getElementById("enterButton");
-    const inputElement = document.getElementById("inputElement");
-    const savebutton = document.getElementById("savebutton");
-    const loadbutton = document.getElementById("loadbutton");
-    savebutton.addEventListener("click", saveButtonHandler);
-    loadbutton.addEventListener("click", loadButtonHandler);
-    function saveButtonHandler() {
-        let speicher = [];
-        let arrayLocalStorage;
-        let arrayString = JSON.stringify(speicher);
-        function loadButtonHandler() {
-            let valueFromLocalStorage = localStorage.getItem("gis_praktikum_input");
-            console.log("aktuelle Wert im Local Storage: " + valueFromLocalStorage);
-            display.textContent = valueFromLocalStorage;
-            localStorage.setItem("myArray", arrayString);
-            let stringLocalStorage = localStorage.getItem("myArray");
-            arrayLocalStorage = JSON.parse(stringLocalStorage);
+    myButton.addEventListener("click", mybuttonHandler);
+    let events = [];
+    class Event {
+        interpret;
+        price;
+        constructor(interpret, price) {
+            this.interpret = interpret;
+            this.price = price;
         }
-        myButton.addEventListener("click", mybuttonHandler);
-        function mybuttonHandler() {
-            let interpretValue = interpretInput.value;
-            let priceValue = Number(priceInput.value);
-            let deleteButton = document.createElement("button");
-            deleteButton.textContent = "Delete";
-            const newZeile = document.createElement("tr");
-            const newInterpret = document.createElement("td");
-            newInterpret.textContent = interpretValue;
-            const newPrice = document.createElement("td");
-            newPrice.textContent = String(priceValue);
-            deleteButton.addEventListener("click", deleteEvent);
-            display.appendChild(newZeile);
-            newZeile.appendChild(newInterpret);
-            newZeile.appendChild(newPrice);
-            newZeile.appendChild(deleteButton);
-            function deleteEvent() {
-                newZeile.removeChild(newInterpret);
-                newZeile.removeChild(newPrice);
-                newZeile.removeChild(deleteButton);
-            }
+        set interpretName(name) {
+            this.interpret = name;
+        }
+        get interpretName() {
+            return this.interpret;
+        }
+        set priceZahl(price) {
+            this.price = price;
+        }
+        get priceZahl() {
+            return this.price;
+        }
+    }
+    load();
+    function mybuttonHandler() {
+        let interpretValue = interpretInput.value;
+        let priceValue = Number(priceInput.value);
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        const newZeile = document.createElement("tr");
+        const newInterpret = document.createElement("td");
+        newInterpret.textContent = interpretValue;
+        const newPrice = document.createElement("td");
+        newPrice.textContent = String(priceValue);
+        deleteButton.addEventListener("click", deleteEvent);
+        display.appendChild(newZeile);
+        newZeile.appendChild(newInterpret);
+        newZeile.appendChild(newPrice);
+        newZeile.appendChild(deleteButton);
+        function deleteEvent() {
+            newZeile.removeChild(newInterpret);
+            newZeile.removeChild(newPrice);
+            newZeile.removeChild(deleteButton);
+        }
+        save();
+    }
+    function save() {
+        let arrayString = JSON.stringify(events);
+        localStorage.setItem("event", arrayString);
+    }
+    function load() {
+        let stringFromLocalStorage = localStorage.getItem("event");
+        let arrayIGotFromStorage = JSON.parse(stringFromLocalStorage);
+        for (let event of arrayIGotFromStorage) {
+            events[events.length] = event;
         }
     }
 })(Eventtabelle || (Eventtabelle = {}));
--- Aufgabe4/script.ts
+++ Aufgabe4/script.ts
@@ -4,68 +4,77 @@ namespace Eventtabelle {
   const priceInput: HTMLInputElement = <HTMLInputElement>document.getElementById("priceInput");
   const display: HTMLElement = <HTMLElement>document.getElementById("display");
   const myButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("enterButton");
-  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById("inputElement");
-  const savebutton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("savebutton");
-  const loadbutton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("loadbutton");
+  myButton.addEventListener("click", mybuttonHandler);
+  let events: Event[] = [];
 
-
-  savebutton.addEventListener("click", saveButtonHandler);
-  loadbutton.addEventListener("click", loadButtonHandler);
-
-  function saveButtonHandler(): void {
-    let speicher: number[] = [];
-    let arrayLocalStorage: number[];
-    let arrayString: string = JSON.stringify(speicher)
-
-
-    function loadButtonHandler(): void {
-      let valueFromLocalStorage: string = localStorage.getItem("gis_praktikum_input");
-      console.log("aktuelle Wert im Local Storage: " + valueFromLocalStorage);
-      display.textContent = valueFromLocalStorage;
-      localStorage.setItem("myArray", arrayString);
-      let stringLocalStorage: string = localStorage.getItem("myArray")
-      arrayLocalStorage = JSON.parse(stringLocalStorage);
-
-
+  class Event {
+    interpret: string;
+    price: number;
+    constructor(interpret: string, price: number) {
+      this.interpret = interpret;
+      this.price = price;
     }
-
-
-
-    myButton.addEventListener("click", mybuttonHandler);
-
-
-    function mybuttonHandler() {
-      let interpretValue: string = interpretInput.value;
-      let priceValue: number = Number(priceInput.value);
-      let deleteButton: HTMLButtonElement = document.createElement("button");
-      deleteButton.textContent = "Delete";
-
-
-
-      const newZeile: HTMLTableRowElement = document.createElement("tr")
-      const newInterpret: HTMLTableCellElement = document.createElement("td");
-      newInterpret.textContent = interpretValue;
-      const newPrice: HTMLTableCellElement = document.createElement("td");
-      newPrice.textContent = String(priceValue);
-
-      deleteButton.addEventListener("click", deleteEvent);
-
-
-      display.appendChild(newZeile);
-      newZeile.appendChild(newInterpret);
-      newZeile.appendChild(newPrice);
-      newZeile.appendChild(deleteButton);
-
-
-      function deleteEvent(): void {
-        newZeile.removeChild(newInterpret);
-        newZeile.removeChild(newPrice);
-        newZeile.removeChild(deleteButton);
-
-
-      }
-
+    set interpretName(name: string) {
+      this.interpret = name;
+    }
+    get interpretName(): string {
+      return this.interpret;
+    }
+    set priceZahl(price: number) {
+      this.price = price;
+    }
+    get priceZahl(): number {
+      return this.price;
     }
   }
+  load();
+  
 
-}
+
+  function mybuttonHandler(): void {
+    let interpretValue: string = interpretInput.value;
+    let priceValue: number = Number(priceInput.value);
+    let deleteButton: HTMLButtonElement = document.createElement("button");
+    deleteButton.textContent = "Delete";
+
+
+
+    const newZeile: HTMLTableRowElement = document.createElement("tr");
+    const newInterpret: HTMLTableCellElement = document.createElement("td");
+    newInterpret.textContent = interpretValue;
+    const newPrice: HTMLTableCellElement = document.createElement("td");
+    newPrice.textContent = String(priceValue);
+
+    deleteButton.addEventListener("click", deleteEvent);
+
+
+    display.appendChild(newZeile);
+    newZeile.appendChild(newInterpret);
+    newZeile.appendChild(newPrice);
+    newZeile.appendChild(deleteButton);
+
+
+    function deleteEvent(): void {
+      newZeile.removeChild(newInterpret);
+      newZeile.removeChild(newPrice);
+      newZeile.removeChild(deleteButton);
+
+
+    }
+    save();
+
+  }
+
+  function save(): void {
+    let arrayString: string = JSON.stringify(events);
+    localStorage.setItem("event", arrayString);
+  }
+
+
+  function load(): void {
+    let stringFromLocalStorage: string = localStorage.getItem("event");
+    let arrayIGotFromStorage: Event[] = JSON.parse(stringFromLocalStorage);
+    for (let event of arrayIGotFromStorage) {
+      events[events.length] = event;
+    }
+  }}
