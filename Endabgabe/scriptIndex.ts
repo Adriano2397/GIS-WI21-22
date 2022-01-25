d46029f87c0afb8475d3cd485c10a20cfa6c26f7
Adriano2397 <adriano.cappelli@hs-furtwangen.de>
2022-01-25 17:41:10 +0100

    Endgame

---
+++ Endabgabe/scriptIndex.ts
@@ -0,0 +1,100 @@
+namespace ProduktTabelle {
+
+    const inputName: HTMLInputElement = <HTMLInputElement>document.getElementById("Name");
+    const inputAblaufdatum: HTMLInputElement = <HTMLInputElement>document.getElementById("Ablaufdatum"); 
+    const inputNotiz: HTMLInputElement = <HTMLInputElement>document.getElementById("Notiz");  
+    const display: HTMLElement = <HTMLElement>document.getElementById("display"); 
+    const updatebutton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("updatebutton");
+    updatebutton.addEventListener("click", update);
+    get();
+    
+    interface Produkte {
+        Name: string;
+        Ablaufdatum: Date;
+        Notiz: string;
+        Eingabedatum: Date;
+    }
+    
+    function update(): void {
+        // tslint:disable-next-line: variable-name
+        let Namevalue: string = inputName.value;
+        // tslint:disable-next-line: variable-name
+        let Ablaufdatumvalue: Date = new Date(inputAblaufdatum.value);
+        // tslint:disable-next-line: variable-name
+        let Notizvalue: string = inputNotiz.value;
+        // tslint:disable-next-line: variable-name
+        let Eingabedatumvalue: Date = new Date();
+  
+    
+        const newNameElement: HTMLTableCellElement = document.createElement("td"); 
+        newNameElement.textContent = Namevalue;
+        const newPriceElement: HTMLTableCellElement = document.createElement("td");
+        newPriceElement.textContent = Ablaufdatumvalue.toLocaleDateString();
+        const newNotizElement: HTMLTableCellElement = document.createElement("td"); 
+        newNotizElement.textContent = Notizvalue;
+        const newEingabedatumElement: HTMLTableCellElement = document.createElement("td"); 
+        newEingabedatumElement.textContent = Eingabedatumvalue.toLocaleDateString();
+        
+    
+        const newReihe: HTMLTableRowElement = document.createElement("tr"); 
+      
+        display.appendChild(newReihe);
+        newReihe.appendChild(newNameElement);
+        newReihe.appendChild(newPriceElement);
+        newReihe.appendChild(newNotizElement);
+        newReihe.appendChild(newEingabedatumElement);
+    
+        let produkt: Produkte = {
+          Name: Namevalue, 
+          Ablaufdatum: Ablaufdatumvalue,
+          Notiz: Notizvalue,
+          Eingabedatum: Eingabedatumvalue
+        };
+        post(produkt);
+      
+    }
+    
+    async function post(produkt: Produkte): Promise<void> {
+      console.log(produkt);
+      await fetch("http://localhost:3000/concertEvents", {
+        method: "POST",
+        body: JSON.stringify(produkt)
+      });
+    }
+    
+    async function get(): Promise<void> {
+      let response: Response = await fetch("http://localhost:3000/concertEvents", {
+        method: "GET"
+      });
+      let antwort: string = await response.text();
+      let produkte: Produkte[] = JSON.parse(antwort) as Produkte[];
+      for (let produkt of produkte) {
+        ausgabe(produkt);
+      }
+    }
+    
+    function ausgabe(produkt: Produkte): void {
+      let newName: string = produkt.Name;
+      let newAblaufdatum: Date = produkt.Ablaufdatum;
+      let newNotiz: string = produkt.Notiz;
+      let newEingabedatum: Date = produkt.Eingabedatum;
+  
+    
+      const newNameElement: HTMLTableCellElement = document.createElement("td"); 
+      newNameElement.textContent = newName;
+      const newPriceElement: HTMLTableCellElement = document.createElement("td");
+      newPriceElement.textContent = String(newAblaufdatum);
+      const newNotizElement: HTMLTableCellElement = document.createElement("td"); 
+      newNotizElement.textContent = newNotiz;
+      const newEingabedatumElement: HTMLTableCellElement = document.createElement("td"); 
+      newEingabedatumElement.textContent = String(newEingabedatum);
+      
+      const newReihe: HTMLTableRowElement = document.createElement("tr"); 
+    
+      display.appendChild(newReihe);
+      newReihe.appendChild(newNameElement);
+      newReihe.appendChild(newPriceElement);
+      newReihe.appendChild(newNotizElement);
+      newReihe.appendChild(newEingabedatumElement);
+    }
+  }
---
+++ Endabgabe/server.js
@@ -0,0 +1,57 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http = require("http");
+const mongo = require("mongodb");
+const hostname = "127.0.0.1"; // localhost
+const port = 3000;
+const mongoUrl = "mongodb://localhost:27017"; // für lokale MongoDB
+let mongoClient = new mongo.MongoClient(mongoUrl);
+async function dbFind(db, collection, requestObject, response) {
+    let result = await mongoClient
+        .db(db)
+        .collection(collection)
+        .find(requestObject)
+        .toArray();
+    // console.log(result, requestObject); // bei Fehlern zum Testen
+    response.setHeader("Content-Type", "application/json");
+    response.write(JSON.stringify(result));
+}
+async function dbAddOrEdit(db, collection, request) {
+    let jsonString = "";
+    request.on("data", data => {
+        jsonString += data;
+    });
+    request.on("end", async () => {
+        await mongoClient.connect();
+        //console.log(jsonString); // bei Fehlern zum Testen
+        let produkt = JSON.parse(jsonString);
+        produkt._id = undefined;
+        mongoClient.db(db).collection(collection).insertOne(produkt);
+    });
+}
+const server = http.createServer(async (request, response) => {
+    response.statusCode = 200;
+    response.setHeader("Access-Control-Allow-Origin", "*"); // bei CORS Fehler
+    let url = new URL(request.url || "", `http://${request.headers.host}`);
+    switch (url.pathname) {
+        case "/concertEvents": {
+            await mongoClient.connect();
+            switch (request.method) {
+                case "GET":
+                    await dbFind("db", "Produkte", {}, response);
+                    break;
+                case "POST":
+                    await dbAddOrEdit("db", "Produkte", request);
+                    break;
+            }
+            break;
+        }
+        default:
+            response.statusCode = 404;
+    }
+    response.end();
+});
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+//# sourceMappingURL=server.js.map
